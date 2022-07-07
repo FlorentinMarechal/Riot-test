90a0f727799848d48ad3b37fc85bf8a8fbd4f1a3
Florentin Marechal <marechalflorentin@gmail.com>
2022-07-08 00:04:40 +0200

    Data stockées

--- riotApi.js
+++ riotApi.js
@@ -1,12 +1,11 @@
 import fetch from "node-fetch";
 
 const config = {
-    name: "KC Flooo",
     apiKey: "RGAPI-971fca2e-7a7d-4b88-81f1-9f4c8c18f96b"
 }
 
-const getPuuid = async () => {
-    const response = await fetch(`https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-name/${config.name}?api_key=${config.apiKey}`);
+const getPuuid = async (name) => {
+    const response = await fetch(`https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-name/${name}?api_key=${config.apiKey}`);
     const data = await response.json();
     // console.log("puuid", data);
     return data.puuid;
@@ -14,20 +13,23 @@ const getPuuid = async () => {
 
 // getSummoner();
 
-const getMatchList = async () => {
-    const puuid = await getPuuid()
+/* 
+Remplacer les appels à fonction par des paramétres 
+*/
+
+const getMatchList = async (puuid) => {
     const response = await fetch(`https://europe.api.riotgames.com/tft/match/v1/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=${config.apiKey}`);
     const data = await response.json();
     // console.log("match id", data);
     return data;
 }
 
-const getMatchData = async () => {
-    const matchList = await getMatchList();
-    const puuid = await getPuuid();
-    const response = await fetch(`https://europe.api.riotgames.com/tft/match/v1/matches/${matchList[0]}?api_key=${config.apiKey}`)
+const getMatchData = async (matchId, puuid) => {
+    // const matchList = await getMatchList();
+    // const puuid = await getPuuid();
+    const response = await fetch(`https://europe.api.riotgames.com/tft/match/v1/matches/${matchId}?api_key=${config.apiKey}`)
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     const gameInfo = {
         traits:"",
         units:"",
@@ -35,14 +37,35 @@ const getMatchData = async () => {
     }
     // console.log(gameInfo);
     const matchData = data.info.participants;
-    console.log("matchData: ",matchData);
+    // console.log("matchData: ",matchData);
     matchData.forEach(match => {
         if(match.puuid !== puuid) return;
         gameInfo.traits = match.traits;
         gameInfo.units = match.units;
     });
-    console.log("gameInfo: ", gameInfo);
-   
+    // console.log("gameInfo: ", gameInfo);
+    return gameInfo;
 }
 
-getMatchData();
+const name = "KC Flooo"
+
+const puuid = await getPuuid(name);
+
+const matchList = await getMatchList(puuid);
+
+const historicInfo = [];
+
+// matchList.forEach(matchId => {
+//     const gameInfo = await getMatchData(matchId, puuid);
+//     historicInfo.push(gameInfo);
+// })
+
+const createHistoricArray = async () => {
+    for(const matchId of matchList) {
+        const gameInfo = await getMatchData(matchId, puuid);
+        historicInfo.push(gameInfo);
+    }
+    console.log("final data: ", historicInfo);
+}
+
+createHistoricArray();
